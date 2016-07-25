c22523e9e3c5fe33366edd120e675a5d503c9842
Orange Owl <orangeowlsolutions@gmail.com>
2016-07-25 22:05:34 +0200

    Create Functional.cuh

---
+++ Particle-Swarm-Optimization/Functional.cuh
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONAL_CUH
+#define FUNCTIONAL_CUH
+
+/************************/
+/* RASTRIGIN FUNCTIONAL */
+/************************/
+__device__ float rastrigin(float);
+
+/*********************/
+/* SPHERE FUNCTIONAL */
+/*********************/
+__device__ float sphere(float);
+
+#endif
